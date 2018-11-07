af0086e03ef78dfdb17f489569a63186e0c5b724
jc <randomboolean@gmail.com>
2018-11-07 15:19:18 +0100

    license and typo

--- notebooks/warpscript_cellmagic.py
+++ notebooks/warpscript_cellmagic.py
@@ -1,3 +1,19 @@
+#
+#   Copyright 2018  SenX S.A.S.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
 """Jupyter extension that contains a cell magic to execute Warpscript code.
 """
 
@@ -80,7 +96,7 @@ class WarpscriptMagics(Magics):
                 help='If flag is used, overwrite existing stack stored under used variable with a new one.')
     @argument('--address', '-a',
                 default=DEFAULT_ADDRESS,
-                help='The ip address of the gateway connected to the Warp10 platform or WarpScript module. Default to 127.0.0.1.')
+                help='The ip address of the gateway connected to the Warp 10 platform or WarpScript module. Default to 127.0.0.1.')
     @argument('--port', '-p',
                 default=DEFAULT_PORT,
                 help='The corresponding port of the gateway. Default to 25333.')
