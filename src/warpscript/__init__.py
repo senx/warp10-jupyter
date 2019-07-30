53008baf59c3379a4c987d2c039422488ccdd64d
jc <jean-charles.vialatte@senx.io>
2019-07-30 17:10:50 +0200

    newLocalStack() and newStack() functions

--- src/warpscript/__init__.py
+++ src/warpscript/__init__.py
@@ -24,8 +24,11 @@ standard_library.install_aliases()
 
 from py4j import protocol as proto
 from py4j.protocol import register_output_converter
+from py4j.java_gateway import launch_gateway, JavaGateway, GatewayParameters
 from .warpscript_magics import WarpscriptMagics
-from .warp10_gateway import convert
+from .warpscript_magics import DEFAULT_GATEWAY_ADDRESS, DEFAULT_GATEWAY_PORT
+from .warp10_gateway import convert, DEFAULT_LOCAL_LAUNCH_CONF
+from .warp10_version import get_Warp10_jar_path
 
 def load_ipython_extension(ipython):
     """Allow this package to be loaded as a Jupyter extension.
@@ -39,3 +42,21 @@ def load_ipython_extension(ipython):
 register_output_converter(
     proto.REFERENCE_TYPE, lambda target_id, gateway_client:
     convert(target_id, gateway_client))
+
+# Create stack not associated to a notebook or to a variable name
+# Each of these stacks creates a new gateway connection
+def newStack(addr=DEFAULT_GATEWAY_ADDRESS, port=DEFAULT_GATEWAY_PORT, auth_token=None):
+    """Creates a new stack from a remote gateway spawned by a Warp 10 platform
+    """
+
+    gateway = JavaGateway(gateway_parameters=GatewayParameters(addr, port, auto_convert=True, auth_token=auth_token))
+    return gateway.entry_point.newStack()
+
+def newLocalStack(conf=DEFAULT_LOCAL_LAUNCH_CONF, classpath=get_Warp10_jar_path()):
+    """Creates a new stack locally (not connected by a Warp 10 platform)
+    """
+
+    port, auth_token = launch_gateway(enable_auth=True,die_on_exit=True,classpath=classpath)
+    gateway = JavaGateway(gateway_parameters=GatewayParameters(port=port, auto_convert=True, auth_token=auth_token))
+    entry_point = gateway.jvm.io.warp10.Py4JEntryPoint(conf)
+    return entry_point.newStack()
